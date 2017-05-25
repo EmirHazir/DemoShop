ba6d85a70b284ba9ab8e44b2c550fc4dfcc43f14
EmirHazir <emir-_-hazir@hotmail.com>
2017-05-25 12:06:21 +0300

    Roles on GlobalAsax, _Layout fixed for admin area

--- DemoShop/Global.asax.cs
+++ DemoShop/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DemoShop.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,30 @@ namespace DemoShop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        //Roller 
+        protected void Application_AuthenticateRequest()
+        {
+            if (User == null) { return; }
+
+            string userName = Context.User.Identity.Name;
+
+            string[] roles = null;
+
+            using (DContext db = new DContext())
+            {
+                UserDTO dto = db.Users.FirstOrDefault(x => x.UserName == userName);
+
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id)
+                        .Select(x => x.Role.RoleName).ToArray();
+            }
+
+            IIdentity userIdentity = new GenericIdentity(userName);
+
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            Context.User = newUserObj;    
         }
     }
 }
