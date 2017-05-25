2c64a75f483a6818886c618a3eca0578fe993d64
EmirHazir <emir-_-hazir@hotmail.com>
2017-05-25 12:26:54 +0300

    Dcontext added overwrite, OrderDTO, OrderDetailsDTO is done, update-database             modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
    is done

--- DemoShop/Models/Data/DContext.cs
+++ DemoShop/Models/Data/DContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,12 @@ namespace DemoShop.Models.Data
 {
     public class DContext : DbContext
     {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
         public DbSet<CategoryDTO> Categories { get; set; }
@@ -15,5 +22,7 @@ namespace DemoShop.Models.Data
         public DbSet<UserDTO> Users { get; set; }
         public DbSet<RoleDTO> Roles { get; set; }
         public DbSet<UserRolesDTO> UserRoles { get; set; }
+        public DbSet<OrderDTO> Orders { get; set; }
+        public DbSet<OrderDetailsDTO> OrderDetails { get; set; }
     }
 }
